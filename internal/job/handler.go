8279296f28ad08f5d60b28e563820099b1dc2612
AbraTM <tusharmalhan2564@gmail.com>
2026-04-15 18:17:23 +0000

    job HandlerFun

--- internal/job/handler.go
+++ internal/job/handler.go
@@ -1,7 +1,15 @@
 package job
 
-import "context"
+import (
+	"context"
+)
 
 type Handler interface {
 	Handle(ctx context.Context, job Job) error
+}
+
+type HandlerFunc func(ctx context.Context, j Job) error
+
+func (f HandlerFunc) Handle(ctx context.Context, j Job) error {
+	return f(ctx, j)
 }
